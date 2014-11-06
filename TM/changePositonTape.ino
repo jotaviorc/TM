263045bd6c05216b27450d2c6e0d0b731c54999d
João Otávio <jotaviorc@yahoo.com.br>
2014-11-05 22:19:44 -0200

    Update changePositonTape.ino

--- TM/changePositonTape.ino
+++ TM/changePositonTape.ino
@@ -1,19 +1,33 @@
-void changePositonTape(int aux)
+// --- Modifica POSIÇÃO atual conforme AUXILIAR --- //
+
+void changePositionTape(int aux, int auxMenu)
 {
   digitalWrite(positionTape, LOW);
   positionTape = positionTape + aux;
-  if (positionTape < STARTPOS) positionTape = STOPPOS;
-  if (positionTape > STOPPOS) positionTape = STARTPOS;
-  digitalWrite(positionTape, HIGH);
+  if (auxMenu)
+  {
+    if (positionTape < STARTPOS) positionTape = STOPPOS;
+    if (positionTape > STOPPOS) positionTape = STARTPOS;
+    digitalWrite(positionTape, HIGH);
+  }
+  else
+  {
+    if (positionTape >= STARTPOS || positionTape <= STOPPOS) digitalWrite(positionTape, HIGH);
+  }
 }
+
+// --- Modifica o dado da FITA conforme POSIÇÃO e grava o valor no inputTape. Leitura do Dado via VALEU --- //
 
 void configInputTape(void)
 {
   int aux = positionTape - STARTPOS;
-  inputTape[aux] = digitalRead(VALUE);
+  inputTape[aux] = inputTape[aux] == 1? 0: 1;
   debugTape();
+  Serial.println();
   digitalWrite(STARTTAPE + aux, inputTape[aux]);
 }
+
+// --- Modifica ESTADO atual conforme AUXILIAR, apenas quando TESTE estiver em "1" --- //
 
 void changeState(int aux)
 {
@@ -24,12 +38,16 @@ void changeState(int aux)
   digitalWrite(state, HIGH);
 }
 
+// --- Escreve DADO da POSIÇÃO da FITA na SAIDA entre STARTTAPE e STOPTAPE --- //
+
 void writeTape(void)
 {
   digitalWrite((STARTTAPE + (positionTape - STARTPOS)), inputTape[positionTape - STARTPOS]);
 }
 
+// --- Escreve dados de toda FITA na SAIDA SERIAL --- //
+
 void debugTape(void)
 {
-  for (int i; i <= 11; i ++)  Serial.print(inputTape[i]);
+  for (int count = 0; count <= STOPTAPE - STARTTAPE; count ++)  Serial.print(inputTape[count]);
 }
